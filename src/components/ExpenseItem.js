0bea3963dbcb165a250d62f19462a17cd5411ef7
Süleyman Yılmaz <suleyman.yilmaz@odeontechnology.com>
2022-09-08 17:19:26 +0300

    Adding css file to first component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <h2>Expense item!</h2>
+        <div className='expense-item'>
+            <div>08.09.2022</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$294.91</div>
+            </div>
+        </div>
     );
 }
 
